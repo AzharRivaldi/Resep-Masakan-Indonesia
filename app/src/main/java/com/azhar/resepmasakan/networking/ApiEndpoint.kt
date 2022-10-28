ec329de03fcae22d952ecfc21cd4e0003d68f09d
Azhar Rivaldi <46733833+AzharRivaldi@users.noreply.github.com>
2022-10-28 21:46:28 +0700

    Update ApiEndpoint.kt

--- app/src/main/java/com/azhar/resepmasakan/networking/ApiEndpoint.kt
+++ app/src/main/java/com/azhar/resepmasakan/networking/ApiEndpoint.kt
@@ -7,8 +7,8 @@ package com.azhar.resepmasakan.networking
 object ApiEndpoint {
     var BASEURL = "https://masak-apa-tomorisakura.vercel.app"
     var PAGE = "/api/recipes/{page}"
-    var CATEGORIES = "/api/categorys/recipes"
-    var LIST_CATEGORIES = "/api/categorys/recipes/{key}"
+    var CATEGORIES = "/api/category/recipes"
+    var LIST_CATEGORIES = "/api/category/recipes/{key}"
     var DETAIL_RECIPES = "/api/recipe/{key}"
     var SEARCH_RECIPES = "/api/search/?q={query}"
 }
